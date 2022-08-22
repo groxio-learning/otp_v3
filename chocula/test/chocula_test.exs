9e33832c991d13ea4c6783f14a8e11f6c99dab98
Steve Foley <steve.f@verypossible.com>
2022-08-22 07:43:59 -0700

    Add map handling

--- chocula/test/chocula_test.exs
+++ chocula/test/chocula_test.exs
@@ -2,7 +2,13 @@ defmodule ChoculaTest do
   use ExUnit.Case
   doctest Chocula
 
-  test "greets the world" do
-    assert Chocula.hello() == :world
+  alias Chocula.Core.Map
+
+  test "make map increment from count" do
+    assert Map.construct("42") |> Map.accumulate(2) |> Map.display() == "The count is 44"
+  end
+
+  test "make map decrement from count" do
+    assert Map.construct("42") |> Map.accumulate(-2) |> Map.display() == "The count is 40"
   end
 end
