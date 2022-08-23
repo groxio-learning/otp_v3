9a2335133c167e70d45bab51fde1933868aff148
Chris Mack <chris.m@verypossible.com>
2022-08-23 14:27:57 -0400

    Refactored history

--- spies/lib/spies/core/history.ex
+++ spies/lib/spies/core/history.ex
@@ -1,35 +1,30 @@
 defmodule Spies.Core.History do
+  defstruct [answer: [1,2,3,4], guesses: []]
 
   def new(opts, size \\ 4)
   def new({:repeat, :random}, size) do
-    %{}
-    |> Map.put(:guesses, [])
-    |> Map.put(:answer, generate_random_list_with_repeats(size))
+    %__MODULE__{answer: generate_random_list_with_repeats(size)}
   end
 
   def new({:random}, size) do
-    %{}
-    |> Map.put(:guesses, [])
-    |> Map.put(:answer, generate_random_list_no_repeats(size))
+    %__MODULE__{answer: generate_random_list_no_repeats(size)}
   end
 
   def new({}, answer) do
-    %{}
-    |> Map.put(:guesses, [])
-    |> Map.put(:answer, answer)
+    %__MODULE__{answer: answer}
   end
 
   defp generate_random_list_no_repeats(size) do
-    Enum.take_random(0..9, size)
+    Enum.take_random(1..8, size)
   end
 
   defp generate_random_list_with_repeats(size) do
-    Enum.reduce(1..size, [], fn _, acc -> acc ++ [Enum.random(0..9)] end)
+    Stream.repeatedly(fn -> Enum.random(1..8) end)
+    |> Enum.take(size)
   end
 
-  def guess(state, current_guess) do
-    state
-    |> Map.put(:guesses, [current_guess | state.guesses])
+  def guess(game, current_guess) do
+    %{game | guesses: [current_guess | game.guesses]}
   end
 
 end
