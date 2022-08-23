3f1db1f69cd67791434126c3cd6d9cfceb565ae7
Chris Mack <chris.m@verypossible.com>
2022-08-23 10:36:52 -0400

    Creating spies

---
+++ spies/lib/spies.ex
@@ -0,0 +1,18 @@
+defmodule Spies do
+  @moduledoc """
+  Documentation for `Spies`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Spies.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
