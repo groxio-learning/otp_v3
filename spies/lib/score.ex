8500ac79bfe592ac05200e99c382faa6de619a4e
Steve Foley <steve.f@verypossible.com>
2022-08-23 09:56:07 -0700

    Add support for multiple guesses of the same color

--- spies/lib/score.ex
+++ spies/lib/score.ex
@@ -1,32 +1,59 @@
 defmodule Spies.Score do
   # TODO Handle multiple colors in the same guess better
   def accumulator() do
-    %{red: 0, white: 0}
+    %{red: 0, white: 0, red_colors: []}
   end
 
-  def increment_color(accumulator, color) do
+  def increment_red(accumulator, guess_value) do
     {_, new_accumulator} =
-      Map.get_and_update(accumulator, color, fn value -> {value, value + 1} end)
+      Map.get_and_update(accumulator, :red, fn value -> {value, value + 1} end)
+
+    {_, new_accumulator} =
+      Map.get_and_update(new_accumulator, :red_colors, fn red_color_list ->
+        {red_color_list, red_color_list ++ [guess_value]}
+      end)
 
     new_accumulator
   end
 
-  def check_score(answer, guess) do
-    # answer/guess look like [4, 2, 1, 8]
-    result = accumulator()
+  def increment_white(accumulator) do
+    {_, new_accumulator} =
+      Map.get_and_update(accumulator, :white, fn value -> {value, value + 1} end)
 
-    # reduce across the guess, comparing and incrementing at each location
+    new_accumulator
+  end
+
+  def find_reds(acc, answer, guess) do
     guess
     |> Enum.with_index()
-    |> Enum.reduce(result, fn {guess_item, index}, result_acc ->
+    |> Enum.reduce(acc, fn {guess_item, index}, result_acc ->
       cond do
-        guess_item == Enum.at(answer, index) -> increment_color(result_acc, :red)
-        Enum.member?(answer, guess_item) -> increment_color(result_acc, :white)
+        guess_item == Enum.at(answer, index) -> increment_red(result_acc, guess_item)
         true -> result_acc
       end
     end)
+  end
 
-    # ${red: 0, white: 1}
+  def find_whites(acc, answer, guess) do
+    guess
+    |> Enum.with_index()
+    |> Enum.reduce(acc, fn {guess_item, index}, result_acc ->
+      cond do
+        Enum.member?(answer, guess_item) and
+            not Enum.member?(Map.get(acc, :red_colors), guess_item) ->
+          increment_white(result_acc)
+
+        true ->
+          result_acc
+      end
+    end)
+  end
+
+  def check_score(answer, guess) do
+    # answer/guess look like [4, 2, 1, 8] ${red: 0, white: 1}
+    accumulator()
+    |> find_reds(answer, guess)
+    |> find_whites(answer, guess)
   end
 
   def convert(rw_map) do
